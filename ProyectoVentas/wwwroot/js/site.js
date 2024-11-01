cccc69939d43290e5b44afd2fa81631874b5db03
JoaquinRouge <Joarouge@gmail.com>
2024-11-01 11:39:27 -0300

    Actualizacion de carrito

--- ProyectoVentas/wwwroot/js/site.js
+++ ProyectoVentas/wwwroot/js/site.js
@@ -2,14 +2,17 @@
 const carrito = document.getElementById("carrito")
 const cantProds = document.getElementById("prods")
 const total = document.getElementById("precio")
+
+// Clase Prenda:
 class Prenda {
-    constructor(id, nombre, precio, talle, descripcion, publico) { 
+    constructor(id, nombre, precio, talle, descripcion, publico,cantidad) { 
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
         this.talle = talle;
         this.descripcion = descripcion;
         this.publico = publico;
+        this.cantidad = cantidad;
     }
 
     mismoId(id) {
@@ -19,19 +22,23 @@ class Prenda {
 let cartObjs = [];
 
 //Agrega una Prenda al array cartObjs.
-function agregarCarrito(id, nombre, precio, talle, descripcion, publico) { 
+function agregarCarrito(id, nombre, precio, talle, descripcion, publico,cantidad) { 
 
     cartObjs = JSON.parse(localStorage.getItem("cartObjs"))//Trae el carrito de la localStorage.
     //Chequea que la prenda no exista en el carrito.
     if (!buscarPrenda(id)) {
         //Si no existe la agrega.
-        cartObjs.push(new Prenda(id, nombre, precio, talle, descripcion, publico));
+        cartObjs.push(new Prenda(id, nombre, precio, talle, descripcion, publico,cantidad));
         localStorage.setItem("cartObjs", JSON.stringify(cartObjs)) //Guarda el array en la memoria del navegador.
         counterCart.innerHTML = JSON.parse(localStorage.getItem("cartObjs")).length;// Actualiza el contador.
         generarHtmlCarrito()
     } else {
         //Si existe lo notifica mediante alerta.
+        //
+        //
         //TODO-- Sustituir la alerta por un mensaje en pantalla.
+        //
+        //
         alert("El producto que intentas agregar ya esta en el carrito.")
     }
     actualizarCantidadProductos()
@@ -87,7 +94,8 @@ function generarHtmlCarrito() {
                 </div>
                 <div>
                     <p>Cantidad</p>
-                    <input type="number" id="inp-number" min="1" placeholder="0">
+                    <input type="number" id="inp-number-${prenda.id}" class="inp-number" min="1" 
+                    placeholder="${prenda.cantidad}" value="${prenda.cantidad}">
                 </div>
                 <button onclick="removePrendaCarrito(${prenda.id})">Eliminar</button>
         </div>
@@ -95,7 +103,22 @@ function generarHtmlCarrito() {
         arrDibujado.push(prenda.id) // Una vez dibujada la prenda la agrega al array de dibujados.
         }
     }
-    )}
+    )
+
+    //Agrega el evento para que al incrementar o decrementar el input de cantidad pedida se actualice el precio
+    //total de los productos y la cantidad total de los productos.
+    carritoMemoria.forEach(function (prenda) {
+        let inputCant = document.getElementById(`inp-number-${prenda.id}`) // Selecciona el input desde el body.
+        inputCant.addEventListener("input", function () {
+            prenda.cantidad = inputCant.value // Setea el nuevo valor de cantidad al objeto de la prenda.
+
+            localStorage.setItem("cartObjs", JSON.stringify(carritoMemoria)) // Actualiza la prenda en memoria.
+
+            actualizarPrecioTotal() // Actualiza el precio total.
+            actualizarCantidadProductos() // Actualiza la cantidad de productos.
+        })
+    })
+}
 
 function removePrendaCarrito(id) {
 
@@ -112,22 +135,28 @@ function removePrendaCarrito(id) {
 
 }
 
+// Actualiza la cantidad de productos totales del carrito
 function actualizarCantidadProductos() {
-    let array = JSON.parse(localStorage.getItem("cartObjs"))
-    let cant = 0
-    array.forEach(function () {
-        cant += 1
+    let array = JSON.parse(localStorage.getItem("cartObjs")) // Trae el array de la localStorage
+    let cant = 0 // Inicializa una cantidad base
+
+    //Recorre las prendas del array
+    array.forEach(function (prenda) {
+        cant += parseInt(prenda.cantidad) // Suma cada cantidad por cada prenda a la variable cant
     })
-    cantProds.innerHTML += cant;
+    cantProds.innerHTML ="Productos: " + cant; // Setea el valor del HTML como el valor de la variable cant
 }
 
+//Actualiza el precio total del carrito
 function actualizarPrecioTotal() {
-    let array = JSON.parse(localStorage.getItem("cartObjs"))
-    let precioTotal = 0
+    let array = JSON.parse(localStorage.getItem("cartObjs")) // Trae el array de la localStorage.
+    let precioTotal = 0 // Inicializa un precio base
+
+    //Recorre las prendas del array
     array.forEach(function (prenda) {
-        precioTotal += prenda.precio
-    })
-    total.innerHTML += precioTotal
+        precioTotal += prenda.precio * prenda.cantidad // Suma a precioTotal la multiplicacion de el precio de 
+    })                                                 // la prenda con su cantidad pedida
+    total.innerHTML ="Total: " + precioTotal // Setea el valor del HTML como el valor de la variable precioTotal 
 }
 
 generarHtmlCarrito();//Agrega las tarjetas al carrito.
